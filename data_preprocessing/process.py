7276ada87314f4db35958ba9f1862b23cb596fdf
Liu Dairui <rxhjxja@icloud.com>
2019-06-22 16:39:33 +0800

    encapsulates data pre-processing and provide method for later observe

--- data_preprocessing/process.py
+++ data_preprocessing/process.py
@@ -4,36 +4,59 @@ import os
 from matplotlib import pyplot as plt
 
 
-processed_json_file = 'siri.20121125.json'
-if not os.path.exists(processed_json_file):
-    # no head exist and using ',' as separator, rename the index, set null value as Na
+def get_x_y(test_line: list):
+    lon_x, lat_y = [], []
+    for timestamp in test_line:
+        for point in timestamp.values():
+            lon_x.append(point[0])
+            lat_y.append(point[1])
+    return lon_x, lat_y
+
+
+def plot_trip_in_line(lon_x: list, lat_y: list, title: str):
+    plt.title(title)
+    plt.xlabel('longitude')
+    plt.ylabel('latitude')
+    plt.plot(lon_x, lat_y,  c='r', marker='o', mec='k', mfc='w', linestyle=':')
+    plt.show()
+
+
+# load origin csv file
+def load_data():
+    processed_data = {}
+    # no head exist, using ',' as separator, rename the index, set null value as Na
     raw_data = pd.read_csv('siri.20121125.csv', header=None, sep=',', na_values=['null'], dtype={'lineId': str},
                            names=['timestamp', 'lineId', 'direction', 'journeyId', 'timeFrame', 'vehicleJourneyId',
-                                  'operator',
-                                  'congestion', 'lon', 'lat', 'delay', 'blockId', 'vehicleId', 'stopId', 'atStop']
+                                  'operator', 'congestion', 'lon', 'lat', 'delay', 'blockId', 'vehicleId', 'stopId',
+                                  'atStop']
                            )
-    print(raw_data.head())  # test
-    processed_data = {}
     for row in raw_data.itertuples(index=True, name='Pandas'):
         primary_key = str(getattr(row, 'lineId')) + '#' + str(getattr(row, 'direction')) + '#' + \
                       str(getattr(row, 'vehicleId')) + '#' + str(getattr(row, 'timeFrame'))
         if primary_key not in processed_data:
             processed_data[primary_key] = []
-        processed_data[primary_key].append({getattr(row, 'timestamp'): (getattr(row, 'lon'), getattr(row, 'lat'))})
-        # print(primary_key)
+        processed_data[primary_key].append({getattr(row, 'timestamp'): [getattr(row, 'lon'), getattr(row, 'lat')]})
     with open(processed_json_file, 'w') as f:
         json.dump(processed_data, f, indent=2)
-else:
-    with open(processed_json_file) as f:
-        processed_data = json.load(f)
-# test the bus trip by plotting the line
-test_line = processed_data['27#0#33234#2012-11-24']
-lon_x, lat_y = [], []
-for timestamp in test_line:
-    for point in timestamp.values():
-        lon_x.append(point[0])
-        lat_y.append(point[1])
-plt.plot(lon_x, lat_y, 'ok')
-plt.xlabel('longitude')
-plt.ylabel('latitude')
-plt.show()
+    return processed_data
+
+
+def load_json(json_file):
+    with open(json_file) as f:
+        return json.load(f)
+
+
+def dump_json(data, json_file):
+    with open(json_file, 'w') as f:
+        json.dump(data, f, indent=2)
+
+
+def load_trip():
+    if not os.path.exists(processed_json_file):
+        processed_data = load_data()
+    else:
+        processed_data = load_json(processed_json_file)
+    return processed_data
+
+
+processed_json_file = 'siri.20121125.json'
