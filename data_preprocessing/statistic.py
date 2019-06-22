9df0fbacfbe4b895a2d1ddc375ee8c0f813f926f
Liu Dairui <rxhjxja@icloud.com>
2019-06-22 16:53:58 +0800

    calculate ratio and plot histogram

--- data_preprocessing/statistic.py
+++ data_preprocessing/statistic.py
@@ -12,11 +12,18 @@ def plot_hist(d, bins):
 
 
 processed_data = process.load_trip()
+nan_trip = {}
 trip_count, count, trip_info = [], 0, {'describe': 'lineId#direction#vehicleId#timeFrame#pointsCount'}
 for k, v in processed_data.items():
     primary_key = k.split('#')
+    if primary_key[0] == 'nan' or primary_key[1] == 'nan' or primary_key[2] == 'nan' or primary_key[3] == 'nan':
+        nan_trip[k] = v
     trip_count.extend([count for i in range(len(v))])
     trip_info[count] = k + '#' + str(len(v))
     count += 1
 plot_hist(trip_count, [i for i in range(count)])
 process.dump_json(trip_info, 'trip.info.json')
+process.dump_json(nan_trip, 'nan.20121125.json')
+print('Total trips:', len(processed_data.keys()))
+print('Total nan trips:', len(nan_trip.keys()))
+print('The ratio for missing data is:', len(nan_trip.keys()) / len(processed_data.keys()))
