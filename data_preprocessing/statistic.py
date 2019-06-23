7ad0838edced817b9907ace6ca1f042f9c56a704
Liu Dairui <rxhjxja@icloud.com>
2019-06-23 08:05:03 +0800

    change x,y axis

--- data_preprocessing/statistic.py
+++ data_preprocessing/statistic.py
@@ -5,8 +5,8 @@ from matplotlib import pyplot as plt
 def plot_hist(d, bins):
     plt.hist(x=d, bins=bins, color='#607c8e', alpha=0.7, rwidth=0.85)
     plt.grid(axis='y', alpha=0.75)
-    plt.xlabel('Trip ID')
-    plt.ylabel('Points Count')
+    plt.xlabel('The number of data points')
+    plt.ylabel('The number of trips')
     plt.title('Distribution of the number of GPS points per trip')
     plt.show()
 
@@ -18,10 +18,11 @@ for k, v in processed_data.items():
     primary_key = k.split('#')
     if primary_key[0] == 'nan' or primary_key[1] == 'nan' or primary_key[2] == 'nan' or primary_key[3] == 'nan':
         nan_trip[k] = v
-    trip_count.extend([count for i in range(len(v))])
+    # trip_count.extend([count for i in range(len(v))])
+    trip_count.append(len(v))
     trip_info[count] = k + '#' + str(len(v))
     count += 1
-plot_hist(trip_count, [i for i in range(count)])
+plot_hist(trip_count, 'auto')
 process.dump_json(trip_info, 'trip.info.json')
 process.dump_json(nan_trip, 'nan.20121125.json')
 print('Total trips:', len(processed_data.keys()))
