c3cd44a46cd109674073d7d75ab5461330c08901
Liu Dairui <rxhjxja@icloud.com>
2019-07-01 11:44:36 +0800

    de-noise point using bus stops

--- data_preprocessing/transform.py
+++ data_preprocessing/transform.py
@@ -1,21 +1,25 @@
 import json
 import process
 import split_dataset
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
 
 
 line15_stops = []
-stops_all = set()
+stops_all = dict()
 trip_sequence = json.load(open('trip_sequence.json'))
+stop_location = json.load(open('stops_location.json'))
 trip_num = 0
 for trip in trip_sequence:
     t = []
     stops = trip.split('-')
-    for stop in stops:
-        stop = stop[8:] if stop[8] != '0' else stop[9:]
+    for s in stops:
+        stop = s[8:] if s[8] != '0' else s[9:]
         t.append(stop)
-        stops_all.add(stop)
+        stops_all[stop] = stop_location[s]
     line15_stops.append(t)
-print(len(line15_stops))
 one_day_data = split_dataset.split('2013-01-07', '2013-01-07')
 one_day_data_clean = []
 trips = {}
@@ -26,14 +30,55 @@ index
 '''
 for line in one_day_data:
     stop_id = line[13].split('.')[0]
+    # filter 1: using stop_id
     if stop_id in stops_all:
-        primary = str(line[12]) + '#' + str(line[3])
-        if primary not in trips:
-            trips[primary] = {'lon': [], 'lat': []}
-        trips[primary]['lon'].append(line[8])
-        trips[primary]['lat'].append(line[9])
-        one_day_data_clean.append(line)
-test = trips['33502#00150001']
-process.plot_trip_in_line(test['lon'], test['lat'], title='test')
+        distance = float(process.cal_distance([float(line[9]), float(line[8])], stops_all[stop_id]))
+        # filter 2: using distance to closet stop
+        if distance < 1000:
+            primary = str(line[12]) + '#' + str(line[3])
+            if primary not in trips:
+                trips[primary] = {'lon': [], 'lat': [], 'stop_id': [], 'timestamp': [], 'info': []}
+            trips[primary]['lon'].append(line[8])
+            trips[primary]['lat'].append(line[9])
+            trips[primary]['stop_id'].append(stop_id)
+            trips[primary]['timestamp'].append(line[0])
+            trips[primary]['info'].append('timestamp:' + str(line[0]) + ',stop_id:' + stop_id + ',vehicle_journey_id:' +
+                                          str(line[5]) + ',block_id:' + str(line[11]) + ',distance_stop:' + str(distance))
+            one_day_data_clean.append(line)
+# for k, v in trips.items():
+#     process.plot_trip_in_line(v['lon'], v['lat'], title=k, file='trips/'+k+'.png')
 print(len(trips))
 print(len(one_day_data_clean))
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+test_id = '33498#00150001'
+test = trips[test_id]
+app.layout = html.Div([
+    dcc.Graph(
+        id=test_id,
+        figure={
+            'data': [
+                go.Scatter(
+                    x=test['lon'],
+                    y=test['lat'],
+                    text=test['info'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=test_id
+                )
+            ],
+            'layout': go.Layout(
+                xaxis={'title': 'Latitude'},
+                yaxis={'title': 'Longitude'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
+app.run_server(debug=True)
