c1ea773c91f03372ccaa7c7e9aaa1aaa18106d2c
Liu Dairui <rxhjxja@icloud.com>
2019-07-03 16:12:37 +0800

    plot distance-time relationship diagram

--- data_preprocessing/transform.py
+++ data_preprocessing/transform.py
@@ -1,10 +1,21 @@
 import json
+import random
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+
 import process
 import split_dataset
+
+
+def create_trip(lon, lat, timestamp, stop_id, primary):
+    return {'lon': [lon], 'lat': [lat], 'timestamp': [timestamp], 'travel_time': [0],
+            'travel_distance': [0], 'start_stop': stop_id, 'stop': [stop_id], 'speed': [0],
+            'name': primary, 'end_stop': end_stop[root_stop.index(stop_id)],
+            'info': ['travel time:0,travel distance:0,speed:0,stop id:' + str(stop_id)]}
+
 
 line15_stops = []
 stops_all = dict()
@@ -21,7 +32,7 @@ for trip in trip_sequence:
     line15_stops.append(t)
 one_day_data = split_dataset.split('2013-01-07', '2013-01-07')
 one_day_data_clean = []
-trips, trajectories = {}, {}
+trajectories = {}
 root_stop = [temp[0] for temp in line15_stops]
 end_stop = [temp[-1] for temp in line15_stops]
 print('Root stop:', root_stop)
@@ -40,62 +51,112 @@ for line in one_day_data:
         if distance < 1000:
             primary = str(line[12]) + '#' + str(line[3])
             lon, lat, timestamp = line[8], line[9], line[0]
-            if primary not in trips:
-                trips[primary] = {'lon': [], 'lat': [], 'stop_id': [], 'timestamp': [], 'info': []}
+            if primary not in trajectories:
                 trajectories[primary] = []
             if stop_id in root_stop and (not trajectories[primary] or
-                                         (trajectories[primary] and stop_id != trajectories[primary][-1]['end_stop'])):
-                trajectories[primary].append({'lon': [lon], 'lat': [lat], 'timestamp': [timestamp],
-                                              'travel_distance': [0], 'start_stop': stop_id,
-                                              'end_stop': end_stop[root_stop.index(stop_id)]})
+                                         (trajectories[primary] and (stop_id != trajectories[primary][-1]['end_stop']
+                                                                     or trajectories[primary][-1]['stop'][
+                                                                         -1] in root_stop))):
+                # create trajectory
+                trajectories[primary].append(create_trip(lon, lat, timestamp, stop_id, primary))
             elif trajectories[primary]:
                 tmp = trajectories[primary][-1]
                 traveled_distance = process.cal_distance([tmp['lat'][-1], tmp['lon'][-1]], [lat, lon])
-                tmp['lon'].append(lon)
-                tmp['lat'].append(lat)
-                tmp['timestamp'].append(timestamp)
-                tmp['travel_distance'].append(tmp['travel_distance'][-1] + traveled_distance)
-            trips[primary]['lon'].append(lon)
-            trips[primary]['lat'].append(lat)
-            trips[primary]['stop_id'].append(stop_id)
-            trips[primary]['timestamp'].append(timestamp)
-            trips[primary]['info'].append(
-                'timestamp:' + str(timestamp) + ',stop_id:' + stop_id + ',vehicle_journey_id:' +
-                str(line[5]) + ',block_id:' + str(line[11]) + ',distance_stop:' + str(distance))
-            one_day_data_clean.append(line)
-print('Count of total trips', len(trips))
-print('Count after cleansing', len(one_day_data_clean))
+                delta = (timestamp - tmp['timestamp'][-1]) / 10e5  # second
+                speed = round((traveled_distance / 1000) / (delta / 3600), 2) if delta != 0 else 0
+                # delta time filter
+                if delta > 1000:
+                    for trip_stop in line15_stops:
+                        if (tmp['stop'][-1] in trip_stop and trip_stop.index((tmp['stop'][-1])) < 3) and (
+                                stop_id in trip_stop and trip_stop.index(stop_id) < 10):
+                            trajectories[primary].append(
+                                create_trip(lon, lat, timestamp, root_stop[line15_stops.index(trip_stop)], primary))
+                    # print(tmp['name'].ljust(14, ' '), stop_id, delta, len(tmp['stop']), tmp['stop'])
+                # speed filter
+                elif speed > 120 or speed < 1:
+                    # fast_point.append([tmp['name'].ljust(14, ' '), speed, 'km/h', traveled_distance, 'm', delta, 's'])
+                    continue
+                else:
+                    tmp['lon'].append(lon)
+                    tmp['lat'].append(lat)
+                    tmp['stop'].append(stop_id)
+                    tmp['timestamp'].append(timestamp)
+                    tmp['travel_time'].append(round(tmp['travel_time'][-1] + delta, 3))
+                    tmp['travel_distance'].append(round(tmp['travel_distance'][-1] + traveled_distance, 2))
+                    speed = tmp['speed'][-1] if traveled_distance == 0 else speed
+                    tmp['speed'].append(speed)  # km/h
+                    tmp['info'].append(
+                        'traveled time:' + str(tmp['travel_time'][-1]) + 's,traveled distance:'
+                        + str(tmp['travel_distance'][-1]) + 'm,speed:' + str(speed) + 'km/h,stop id:' + stop_id)
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-test_id = '33498#00150001'
-test_tra = [tmp for tmp in trajectories[test_id] if len(tmp['lon']) > 2]
-print('Trajectories count:', len(test_tra))
+trajectories_filtered = []
+for v in trajectories.values():
+    for t in v:
+        if len(t['lon']) > 100:
+            trajectories_filtered.append(t)
+print('Total number of trajectories:', len(trajectories_filtered))
+test_num = random.randint(0, len(trajectories_filtered) - 15)
 app.layout = html.Div([
-    dcc.Graph(
-        id=test_id,
-        figure={
-            'data': [
-                go.Scatter(
-                    x=test['lon'],
-                    y=test['lat'],
-                    text='travel distance:' + str(test['travel_distance']),
-                    mode='markers',
-                    opacity=0.7,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=test_id
-                ) for test in test_tra
-            ],
-            'layout': go.Layout(
-                xaxis={'title': 'Latitude'},
-                yaxis={'title': 'Longitude'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest'
-            )
-        }
-    )
+    html.Div([
+        html.H1('Distance-Time'),
+        dcc.Graph(
+            id='distance-time',
+            figure={
+                'data': [
+                    go.Scatter(
+                        x=test['travel_distance'],
+                        y=test['travel_time'],
+                        text=test['info'],
+                        mode='lines',
+                        opacity=0.7,
+                        marker={
+                            'size': 15,
+                            'line': {'width': 0.5, 'color': 'white'}
+                        },
+                        # name=test['name']
+                    ) for test in trajectories_filtered],
+                'layout': go.Layout(
+                    xaxis={'title': 'Traveled distance'},
+                    yaxis={'title': 'Traveled time'},
+                    margin={'l': 60, 'b': 40, 't': 10, 'r': 10},
+                    legend={'x': 0, 'y': -2, 'orientation': 'h'},
+                    hovermode='closest',
+                    showlegend=False,
+                    )
+            }
+        )
+    ]),
+    # html.Div([
+    #     html.Div([
+    #         html.H1('trip no.{}'.format(trajectories_filtered.index(test))),
+    #         dcc.Graph(
+    #             id='trip no.{}'.format(trajectories_filtered.index(test)),
+    #             figure={
+    #                 'data': [
+    #                     go.Scatter(
+    #                         x=test['lon'],
+    #                         y=test['lat'],
+    #                         text=test['info'],
+    #                         mode='markers',
+    #                         opacity=0.7,
+    #                         marker={
+    #                             'size': 15,
+    #                             'line': {'width': 0.5, 'color': 'white'}
+    #                         },
+    #                         name=test['name']
+    #                     )
+    #                 ],
+    #                 'layout': go.Layout(
+    #                     xaxis={'title': 'Latitude'},
+    #                     yaxis={'title': 'Longitude'},
+    #                     margin={'l': 60, 'b': 40, 't': 10, 'r': 10},
+    #                     legend={'x': 0, 'y': 1},
+    #                     hovermode='closest'
+    #                 )
+    #             }
+    #         )]) for test in trajectories_filtered
+    # ])
 ])
+html.Div()
 app.run_server(debug=True)
