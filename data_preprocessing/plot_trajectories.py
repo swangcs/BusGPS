4b84046858aa7b71e6f27c87346e4106426f56e4
MrTornado24 <554880687@qq.com>
2019-08-13 19:36:57 +0800

    implementation of additive model

--- data_preprocessing/plot_trajectories.py
+++ data_preprocessing/plot_trajectories.py
@@ -1,107 +1,47 @@
-import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 
-import process
+
+def get_scatter(x, y, text, marker: dict, name, opacity=0.7, mode='markers'):
+    return go.Scatter(x=x, y=y, text=text, mode=mode, opacity=opacity, marker=marker, name=name)
 
 
-def get_figure(start_stop):
-    return {
-        'data': [
-            go.Scatter(
-                x=test['travel_distance'],
-                y=test['travel_time'],
-                text=test['info'],
-                mode='lines',
-                opacity=0.7,
-                marker={
-                    'size': 15,
-                    'line': {'width': 0.5, 'color': 'white'}
-                },
-                # name=test['name']
-            ) for test in trajectories[start_stop]],
-        'layout': go.Layout(
-            xaxis={'title': 'Traveled distance'},
-            yaxis={'title': 'Traveled time'},
-            margin={'l': 60, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': -2, 'orientation': 'h'},
-            hovermode='closest',
-            showlegend=False,
-        )
-    }
+def get_layout(xaxis: dict, yaxis: dict, showlegend=False):
+    return go.Layout(xaxis=xaxis, yaxis=yaxis, margin={'l': 60, 'b': 40, 't': 10, 'r': 10},
+                     legend={'x': 0, 'y': -2, 'orientation': 'h'}, hovermode='closest', showlegend=showlegend)
 
 
-# figure for additive model
-
-def get_figure_additive(start_stop):
-    data_ =[go.Scatter(
-                x=test['travel_distance'][::5],
-                y=test['travel_time'][::5],
-                text=test['info'][::5],
-                mode='markers + lines',
-                opacity=0.7,
-                marker={
-                    'size': 7,
-                    'line': {'width': 0.005, 'color': 'white'},
-                },
-                # name=test['name']
-            ) for test in trajectories[start_stop]]
-    data_.append(go.Scatter(
-                x=stop_location,
-                y=y1,
-                mode='markers',
-                opacity=0.7,
-                marker={
-                    'size': 10,
-                    'line': {'width': 0.005, 'color': 'white'},
-                },
-                # name=test['name']
-            ))
-    return {
-        'data': data_,
-        'layout': go.Layout(
-            xaxis={'title': 'Traveled distance'},
-            yaxis={'title': 'Traveled time'},
-            margin={'l': 60, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': -2, 'orientation': 'h'},
-            hovermode='closest',
-            showlegend=False,
-        )
-    }
-
-
-if __name__ == '__main__':
-    trajectories_json, start_json = 'trajectories.json', 'start_stops.json'
-    if not os.path.exists(trajectories_json) or not os.path.exists(start_json):
-        print('run transform.py first')
-    else:
-        trajectories = process.load_json(trajectories_json)
-        start_stops = process.load_json(start_json)
-        point_count = []
-        for values in trajectories.values():
-            for trajectory in values:
-                point_count.append(len(trajectory['lon']))
-        external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-        app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-        app.layout = html.Div([
+def plot(trajectories: dict, trips: dict, port=8050, debug=True, x='lon', y='lat'):
+    point_count = []
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+    trip_marker = {'size': 2, 'line': {'width': 0.01, 'color': 'blue'}}
+    route_marker = {'size': 10, 'line': {'width': 0.3, 'color': 'red'}}
+    start_stops = list(trajectories.keys())
+    data = [[get_scatter(trajectory[x], trajectory[y], trajectory['info'], trip_marker, trajectory['id'], mode='lines')
+             for trajectory in trajectories[start_stop]] for start_stop in start_stops]
+    for start_stop in start_stops:
+        point_count.extend([len(trajectory[x]) for trajectory in trajectories[start_stop]])
+        data[start_stops.index(start_stop)].append(
+            get_scatter(trips[start_stop][x], trips[start_stop][y], trips[start_stop]['stop_id'], route_marker,
+                        start_stop))
+    app.layout = html.Div([
+        html.Div([
             html.Div([
-                html.Div([
-                    html.H1('Travel from {}'.format(stop_id)),
-                    dcc.Graph(id='distance-time-{}'.format(stop_id), figure=get_figure(stop_id))
-                ]) for stop_id in start_stops
-            ]),
-            html.Div([dcc.Graph(id='points-statistic', figure={
-                'data': [go.Histogram(x=point_count)],
-                'layout': go.Layout(
-                    xaxis={'title': 'Number of points'},
-                    yaxis={'title': 'Number of trips'},
-                )
-            })])
-        ])
-        app.run_server(debug=True)
-
-
-
+                dcc.Graph(id=''.format('15'), figure={
+                    'data': d,
+                    'layout': get_layout({'title': x}, {'title': y})
+                }) for d in data
+            ])
+        ]),
+        html.Div([dcc.Graph(id='points-statistic', figure={
+            'data': [go.Histogram(x=point_count)],
+            'layout': go.Layout(
+                xaxis={'title': 'Number of points'},
+                yaxis={'title': 'Number of trips'},
+            )
+        })])
+    ])
+    app.run_server(debug=debug, port=port)
