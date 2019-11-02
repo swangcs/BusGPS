798ab2657dacdd1aeb982cbd919cd26bd60a3ada
MrTornado24 <554880687@qq.com>
2019-11-02 11:55:35 +0800

    update KNN, LR additive model

--- model_training/sun/model_KNN.py
+++ model_training/sun/model_KNN.py
@@ -9,7 +9,7 @@ from scipy.spatial.distance import pdist
 
 # Calculation of Root Mean Squared Error
 def rmse(predictions, targets):
-    return np.sqrt(((predictions - targets) ** 2).mean())
+    return np.sqrt(((predictions-targets) ** 2).mean())
 
 
 # Calculation of euclidean distance between two sequences
@@ -30,7 +30,7 @@ def mean_absolute_percentage_error(actual, predict):
         if actual[i] != 0:
             tmp += math.fabs(actual[i]-predict[i]) / actual[i]
             n += 1
-    return (tmp/n)*100
+    return (tmp / n) * 100
 
 
 # getting k nearest neighbors of one trip
@@ -59,7 +59,7 @@ def getNeighbors(trainingSet, testInstance, k, index):
     return neighbors
 
 
-def predict(neighbor, testInstance, index, k):
+def predict(neighbor, testInstance, k, index):
     """
     :param neighbor: k neighbors
     :param testInstance: one trip used for projection
@@ -71,28 +71,33 @@ def predict(neighbor, testInstance, index, k):
     timeSum = 0
     for i in range(index):
         predictTime.append(testInstance[i])
-    for i in range(len(testInstance) - index):
+    for i in range(len(testInstance)-index):
         for j in range(k):
-            timeSum = timeSum + neighbor[j][index + i] - neighbor[j][index - 1]
+            timeSum = timeSum+neighbor[j][index+i-1]-neighbor[j][index-1]
         timeDiff = timeSum / k
-        predictTime.append(predictTime[index - 1] + timeDiff)
+        predictTime.append(predictTime[index-1]+timeDiff)
         timeSum = 0
     return predictTime
 
 
-def training(trainingSet):
+def training_and_evaluation(trainingSet, testSet):
     """
+    :param testSet:
     :param trainingSet: a list of training sets
     :returns: RMSE, MAE, MAPE
     """
-    for j in range(len(trainingSet)):
-        for i in range(len(trainingSet)-2):
-            testInstance = trainingSet[j]
+    RMSE = []
+    MAE = []
+    MAPE = []
+
+    for j in range(len(testSet)):
+        testInstance = testSet[j]
+        for i in range(len(testInstance)-1):
             result = getNeighbors(trainingSet, testInstance, 25, i+1)
-            predictResult = predict(result, testInstance, i+1, 25)
-            RMSE.append(rmse(np.array(predictResult[i+1:]), np.array(testInstance[i+1:])))
-            MAE.append(mean_absolute_error(testInstance[i+1:], predictResult[i+1:]))
-            MAPE.append(mean_absolute_percentage_error(testInstance[i+1:], predictResult[i+1:]))
+            predictResult = predict(result, testInstance, 25, i+1)
+            RMSE.append(rmse(np.array(predictResult[i:]), np.array(testInstance[i:])))
+            MAE.append(mean_absolute_error(testInstance[i:], predictResult[i:]))
+            MAPE.append(mean_absolute_percentage_error(testInstance[i:], predictResult[i:]))
 
     RMSEu = np.mean(RMSE)
     MAEu = np.mean(MAE)
@@ -100,25 +105,57 @@ def training(trainingSet):
 
     return RMSEu, MAEu, MAPEu
 
-
-f = open('/Users/letv/Desktop/智能交通系统/busgps/travel_time.pkl', 'rb')
-trainingSet = pickle.load(f)
-RMSE = []
-MAE = []
-MAPE = []
-
-
-# Figure 3
-testInstance = trainingSet[13]
-result = getNeighbors(trainingSet, testInstance, 25, 50)
-predictResult = predict(result, testInstance, 50, 25)
-cumDist = np. linspace(0, 14700, 148)
-for x in range(25):
-    result[x][50] = trainingSet[13][50]
-    plt.plot(cumDist[50:], np.array(result[x][50:]), color='red', linestyle='-', linewidth=0.3)
-plt.plot(cumDist, np.array(trainingSet[13]), color='blue', linestyle='-', linewidth=0.5)
-plt.show()
-
+########################################################################################################################
+# f = open('/Users/letv/Desktop/IntelligentTraffic/busgps/travel_time.pkl', 'rb')
+# trainingSet = pickle.load(f)
+# RMSE = []
+# MAE = []
+# MAPE = []
+#
+#
+# # Figure 3
+# testInstance = trainingSet[13]
+# result = getNeighbors(trainingSet, testInstance, 25, 50)
+# predictResult = predict(result, testInstance, 50, 25)
+# cumDist = np. linspace(0, 14700, 148)
+# for x in range(25):
+#     result[x][50] = trainingSet[13][50]
+#     plt.plot(cumDist[50:], np.array(result[x][50:]), color='red', linestyle='-', linewidth=0.3)
+# plt.plot(cumDist, np.array(trainingSet[13]), color='blue', linestyle='-', linewidth=0.5)
+# plt.show()
+#
+#
+# # Figure 4 and Figure 5
+# color = ['b', 'g', 'r', 'c', 'm']
+# label = ['k = 5', 'k = 10', 'k = 15', 'k = 20', 'k = 25']
+# tempMAE = []
+# for x in range(5):
+#     k = 5 * (x + 1)
+#     MAE = []
+#     temp = []
+#     tempMAE.append(temp)
+#     for i in range(len(trainingSet)//200):
+#         testInstance = trainingSet[i]
+#         for j in range(14):
+#             result = getNeighbors(trainingSet, testInstance, k, 10*j+1)
+#             predictResult = predict(result, testInstance, 10*j+1, k)
+#             MAE.append(mean_absolute_error(testInstance[i+1:], predictResult[i+1:]))
+#
+#     for j in range(14):
+#         temp = []
+#         tempMAE[x].append(temp)
+#         for i in range(12):
+#             temp.append(MAE[14*i + j])
+#
+#     for i in range(len(tempMAE[x])):
+#         tempMAE[x][i] = np.mean(tempMAE[x][i])
+#
+# for x in range(5):
+#     cumDist = np. linspace(1, 14, 14)
+#     plt.plot(cumDist, tempMAE[x], label=label[x], color = color[x], linewidth = 0.5, linestyle = '-')
+#
+# plt.legend(loc='upper right')
+# plt.show()
 
 ########################################################################################################################
 
